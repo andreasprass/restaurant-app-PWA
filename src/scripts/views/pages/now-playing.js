dd20763de735f1a8d27271457d17aa0ee05ead7c
andreasprass <andreasprasetya29@gmail.com>
2023-01-10 16:33:24 +0700

    save

--- src/scripts/views/pages/now-playing.js
+++ src/scripts/views/pages/now-playing.js
@@ -4,10 +4,17 @@ import { createRestaurantListTemplate } from '../templates/template-creator';
 const List = {
   async render() {
     return `
+      <div class="hero">
+        <div class="hero__inner">
+          <h1 class="hero__title">Bangun Karirmu Sebagai Developer Profesional</h1>
+          <p class="hero__tagline">Jadilah tuan rumah di negeri sendiri dengan belajar langsung dari para inovator dan
+            developer expert</p>
+        </div>
+      </div>
       <div class="content">
         <h2 class="content__heading">Restaurant List</h2>
         <div id="restos" class="restos">
-
+        
         </div>
       </div>
     `;
--- webpack.common.js
+++ webpack.common.js
@@ -25,7 +25,20 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
+  },
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
